61b65cced5ac45928b2d6cb8b4e53bbdfe72a3bc
kyro <9013ms@gmail.com>
2026-02-03 09:16:23 +0530

    update

---
+++ routers/email.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import auth, email  # <-- Yahan humne aapke folder se import kiya
+
+app = FastAPI()
+
+# Routers ko jodna (Include Routers)
+app.include_router(auth.router)   # Login Page wala router
+app.include_router(email.router)  # Email wala router
+
+# Health check
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
